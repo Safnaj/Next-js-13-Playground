c08dd0fbce455db5abf599635d85cd3ca264e077
Safnaj <safnaj99@gmail.com>
2023-09-02 01:14:24 +0530

    Refactor with Layout & Headers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,26 @@
-import './globals.css'
+import Navbar from "./components/Navbar/Navbar";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <main className='bg-gray-100 min-h-screen w-screen text-black'>
+          <main className='max-w-screen-2xl m-auto bg-white'>
+            <Navbar />
+            {children}
+          </main>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- app/search/components/Header.tsx
+++ app/search/components/Header.tsx
@@ -1,14 +1,9 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
+
 export default function Header() {
   return (
     <div className='text-left text-lg py-3 m-auto flex justify-center'>
-      <input
-        className='rounded  mr-3 p-2 w-[450px]'
-        type='text'
-        placeholder='State, city or town'
-      />
-      <button className='rounded bg-red-600 px-9 py-2 text-white'>
-        Let's go
-      </button>
+      <SearchBar />
     </div>
   );
 }
