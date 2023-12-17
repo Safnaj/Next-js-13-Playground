4cb17b7c80506480dcd25319fa709d4ec31396f5
Safnaj <safnaj99@gmail.com>
2023-12-18 01:08:59 +0530

    Auth & Logout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { useContext } from "react";
+import useAuth from "../../hooks/useAuth";
+import { AuthenticationContext } from "../context/AuthContext";
 import AuthModal from "./AuthModal";
 
 export default function Navbar() {
+  const { data, loading } = useContext(AuthenticationContext);
+  const { signout } = useAuth();
+
   return (
     <nav className='bg-white p-2 flex justify-between'>
       <Link href='' className='font-bold text-gray-700 text-xl'>
@@ -9,10 +17,23 @@ export default function Navbar() {
         OpenTable{" "}
       </Link>
       <div>
-        <div className='flex'>
-          <AuthModal isSignIn />
-          <AuthModal isSignIn={false} />
-        </div>
+        {loading ? null : (
+          <div className='flex'>
+            {data ? (
+              <button
+                className='bg-blue-400 text-white border p-1 px-4 rounded'
+                onClick={signout}
+              >
+                Sign out
+              </button>
+            ) : (
+              <>
+                <AuthModal isSignIn />
+                <AuthModal isSignIn={false} />
+              </>
+            )}
+          </div>
+        )}
       </div>
     </nav>
   );
